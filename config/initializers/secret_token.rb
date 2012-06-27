db8b8ef532ddcdc8dcda3d13b91d8bf6bc9d7c46
mahm <nishimi@sonicgarden.jp>
2012-06-27 12:41:05 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monrobo::Application.config.secret_token = 'b1ae2c2dd295489ed28521719f57b74f00e81751d91a3b50060a23a0d89dacd11e2225297254744b8d93b606856d04d3b6d6899814c92a98ea14f57a9eee12ab'
